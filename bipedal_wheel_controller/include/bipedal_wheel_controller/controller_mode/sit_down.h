7ac3ca7bd349a5c87e3638d1f437b9062f00e72b
1moule <1961466188@qq.com>
2025-09-03 15:05:40 +0800

    Get some variables in constructor.

--- bipedal_wheel_controller/include/bipedal_wheel_controller/controller_mode/sit_down.h
+++ bipedal_wheel_controller/include/bipedal_wheel_controller/controller_mode/sit_down.h
@@ -4,6 +4,9 @@
 
 #pragma once
 
+#include <hardware_interface/joint_command_interface.h>
+#include <control_toolbox/pid.h>
+
 #include "bipedal_wheel_controller/controller_mode/mode_base.h"
 #include "bipedal_wheel_controller/definitions.h"
 
@@ -12,10 +15,16 @@ namespace bipedal_wheel_controller
 class SitDown : public ModeBase
 {
 public:
+  explicit SitDown(const std::vector<hardware_interface::JointHandle*>& joint_handles,
+                   const std::vector<control_toolbox::Pid*>& pid_wheels);
   void execute(BipedalController* controller, const ros::Time& time, const ros::Duration& period) override;
   const char* name() const override
   {
     return "SIT_DOWN";
   }
+
+private:
+  std::vector<hardware_interface::JointHandle*> joint_handles_;
+  std::vector<control_toolbox::Pid*> pid_wheels_;
 };
 }  // namespace bipedal_wheel_controller
--- bipedal_wheel_controller/src/controller_mode/sit_down.cpp
+++ bipedal_wheel_controller/src/controller_mode/sit_down.cpp
@@ -7,6 +7,12 @@
 
 namespace bipedal_wheel_controller
 {
+SitDown::SitDown(const std::vector<hardware_interface::JointHandle*>& joint_handles,
+                 const std::vector<control_toolbox::Pid*>& pid_wheels)
+  : joint_handles_(joint_handles), pid_wheels_(pid_wheels)
+{
+}
+
 void SitDown::execute(BipedalController* controller, const ros::Time& time, const ros::Duration& period)
 {
   if (!controller->getStateChange())
@@ -15,13 +21,11 @@ void SitDown::execute(BipedalController* controller, const ros::Time& time, cons
     controller->setStateChange(true);
   }
 
-  auto joint_handles = controller->getJointHandles();
-  auto pid_wheels = controller->getPidWheels();
   auto x = controller->getLeftState();
   LegCommand left_cmd = { 0, 0, { 0., 0. } }, right_cmd = { 0, 0, { 0., 0. } };
-  double left_wheel_cmd = pid_wheels[0]->computeCommand(joint_handles[0]->getVelocity(), period);
-  double right_wheel_cmd = pid_wheels[1]->computeCommand(joint_handles[1]->getVelocity(), period);
-  setJointCommands(joint_handles, left_cmd, right_cmd, left_wheel_cmd, right_wheel_cmd);
+  double left_wheel_cmd = pid_wheels_[0]->computeCommand(joint_handles_[0]->getVelocity(), period);
+  double right_wheel_cmd = pid_wheels_[1]->computeCommand(joint_handles_[1]->getVelocity(), period);
+  setJointCommands(joint_handles_, left_cmd, right_cmd, left_wheel_cmd, right_wheel_cmd);
 
   if (abs(x(1)) < 0.1 && abs(x(5)) < 0.1 && abs(x(3)) < 0.15)
   {
